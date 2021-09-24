7dbf2b4b0db80ac86ca7e309b6fafef02cb1b144
Mickael Gaspar <migatsar@gmail.com>
2021-09-24 17:16:04 +0200

    Relaunched base django project

--- framework_tutorial/settings.py
+++ framework_tutorial/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*-#cnc25+#parq5q!^o-$d-#q(pdz*nyy=qj+@@!(xt8orde1'
+SECRET_KEY = 'django-insecure-u9%1c9((vd114p^q^_d3%8m#k-sswvdep=lh2l2e63-d)zlf@('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
