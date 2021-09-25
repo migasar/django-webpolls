f88ddc4a8e4cb99fe267a76fac0af34e317014b3
Mickael Gaspar <migatsar@gmail.com>
2021-09-25 19:09:24 +0200

    Refactored with generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.detail import DetailView
 
 from . import views
 
@@ -6,11 +7,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
