b5c1f4ff14e143130577fc28a46874b644709669
Mickael Gaspar <migatsar@gmail.com>
2021-09-25 12:22:35 +0200

    Refactored index function with shortcut 'render'   replacing 'HttpResponse' and 'loader'

--- polls/views.py
+++ polls/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
 
 from .models import Question
 
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {'latest_question_list': latest_question_list}
+    # return HttpResponse(template.render(context, request))
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
