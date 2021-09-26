50433e277781c8fca98912bb3a7c7d940125990a
Mickael Gaspar <migatsar@gmail.com>
2021-09-26 14:59:34 +0200

    Refactored code

--- polls/views.py
+++ polls/views.py
@@ -29,9 +29,7 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+        """ Excludes any questions that aren't published yet """
         result = Question.objects.filter(pub_date__lte=timezone.now())
         return result
 
